9520a8e052714c7deadf4c1777d4ebbeb8b90433
PrasanthMigobucks <servers@migobucks.com>
2021-05-05 14:22:35 +0530

    Add product navigtion

--- src/pages/Auth/components/register-success/register-success.tsx
+++ src/pages/Auth/components/register-success/register-success.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import { Container } from '@material-ui/core';
+import { Button, Container } from '@material-ui/core';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 
 import style from './register-success.module.scss';
+import { Redirect, Route, useHistory } from 'react-router-dom';
 
-interface Props {}
+interface Props { }
 
 function RegisterSuccess(props: Props) {
+
+  const history = useHistory();
+  const navigateNext = () => {
+    history.push("/layout");
+  };
+
+
   return (
     <Container className={style["container"]}>
       <div className={style["sub-container"]}>
@@ -17,6 +25,9 @@ function RegisterSuccess(props: Props) {
         <div className={style["succ-head"]}>Successfully Registered</div>
         <div className={style["succ-body"]}>
           Start improving your <b>Customer Engagement Strategies</b>
+        </div>
+        <div>
+        <Button color="primary" variant="contained" onClick={navigateNext}>Get Started</Button>
         </div>
       </div>
     </Container>
