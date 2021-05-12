55c8c218a73ae8132ec2dcefe5354b105bbab330
PrasanthMigobucks <servers@migobucks.com>
2021-05-12 20:44:08 +0530

    mock data added

---
+++ src/mock data/data.ts
@@ -0,0 +1,47 @@
+
+export const data = [
+    {
+        "displayName": "Quantity Availabe",
+        "dataType": "text",
+        "fieldName": "qantity"
+    },
+    {
+        "displayName": "Shelf Life",
+        "dataType": "list",
+        "fieldName": "shelfLife",
+        "data": [
+            {
+                "value": "1",
+                "name": "1 KG"
+            },
+            {
+                "value": "3",
+                "name": "3 KG"
+            },
+            {
+                "value": "5",
+                "name": "5 KG"
+            }
+        ]
+    },
+    {
+        "displayName": "Max.Retail Price(INR)",
+        "dataType": "number",
+        "fieldName": "mrp"
+    },
+    {
+        "displayName": "Loyalty Point%",
+        "dataType": "number",
+        "fieldName": "loyaltyPoint"
+    },
+    {
+        "displayName": "InStock",
+        "dataType": "number",
+        "fieldName": "inStock"
+    },
+    {
+        "displayName": "Add a key point",
+        "dataType": "array",
+        "fieldName": "keyPoints"
+    }
+]
